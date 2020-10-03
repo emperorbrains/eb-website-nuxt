d91926dc46741624ff346bc11c0f00fd03ebdd0c
Harsh Mendapara <mendaparaharsh02@gmail.com>
2020-10-03 20:57:18 +0530

    add dist

--- dist/_nuxt/7f9b59f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{274:function(t,n,e){"use strict";e.r(n);var c={},l=e(30),component=Object(l.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("contact us")])}),[],!1,null,null,null);n.default=component.exports}}]);
