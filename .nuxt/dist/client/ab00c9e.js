5edaf93fc10f7893b17242cde2370a11825b6449
Harsh Mendapara <mendaparaharsh02@gmail.com>
2020-10-03 19:55:25 +0530

    /case-studies/snoopy-whoopy  remove path

--- .nuxt/dist/client/ab00c9e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{267:function(t,n,e){"use strict";e.r(n);var c={},l=e(32),component=Object(l.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("contact us")])}),[],!1,null,null,null);n.default=component.exports}}]);
